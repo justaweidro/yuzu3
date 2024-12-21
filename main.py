560bdd32a167b364b222fb52ea6a79335e1e2384
ebitiri728 <f.kokori85@gmail.com>
2024-12-21 09:57:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -213,7 +213,7 @@ def home(response: Response, request: Request, yuki: Union[str] = Cookie(None)):
         response.set_cookie("yuki", "True", max_age=60 * 60 * 24 * 7)
         return template("home.html", {"request": request})
     print(check_cokie(yuki))
-    return redirect("/nyanko_a")
+    return redirect("/yuzu")
 
 
 @app.get('/watch', response_class=HTMLResponse)
